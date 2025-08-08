040f273ba2d8af5303aebed2eabd03e5e71e53af
CARI JRL <cari@jrl.com>
2025-08-08 16:50:56 +0200

    fixed cmake import issues

--- thor_math/cmake_config/thor_mathConfig.cmake
+++ thor_math/cmake_config/thor_mathConfig.cmake
@@ -1,10 +1,9 @@
 include(CMakeFindDependencyMacro)
 
-
-## insert here the dependencies
+# Ensure the pinocchio target exists before loading our exported targets:
 find_dependency(Eigen3 REQUIRED Core Dense Geometry)
+find_dependency(pinocchio REQUIRED) # or: find_dependency(pinocchio CONFIG REQUIRED)
 find_dependency(rdyn_core REQUIRED)
-
 include("${CMAKE_CURRENT_LIST_DIR}/thor_mathTargets.cmake")
 
 check_required_components(${PROJECT_NAME})
